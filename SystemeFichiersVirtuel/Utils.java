99f9785f2bc65acd3ecc3f68e8bc719ae96c1e02
Lydéric <lyderic.dewachter@iut-rodez.fr>
2026-09-23 15:18:03 +0100

    Modification de la methode writeInt() dans Utils.java afin qu'elle passe les tests TestRunner.java

--- SystemeFichiersVirtuel/Utils.java
+++ SystemeFichiersVirtuel/Utils.java
@@ -3,17 +3,15 @@ import java.lang.reflect.Array;
 public class Utils {
 
     public static int writeInt(byte[] memory, int offset, int value) {
-        // TODO: Écrire les 4 octets de 'value' dans 'memory'
-        // à partir de 'offset', en big-endian.
-        String valueString = Integer.toString(value);
-        memory[offset] = Array.getByte(memory, value >>> 24);
-        memory[offset + 1] = Array.getByte(memory, value >>> 16);
-        memory[offset + 2] = Array.getByte(memory, value >>> 8);
-        memory[offset + 3] = Array.getByte(memory, value);
-	
-        
-	return 4;
+        memory[offset]     = (byte) (value >>> 24);
+        memory[offset + 1] = (byte) (value >>> 16);
+        memory[offset + 2] = (byte) (value >>> 8);
+        memory[offset + 3] = (byte) value;
+
+        return 4;
     }
+
+
 
     public static int readInt(byte[] memory, int offset) {
         // TODO: Reconstituer le int sur 4 octets.
