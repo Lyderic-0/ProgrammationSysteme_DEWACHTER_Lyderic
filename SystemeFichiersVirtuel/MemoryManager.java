fbd6273a65d626e05c08380013392c3319e8883a
Lydéric <lyderic.dewachter@iut-rodez.fr>
2026-09-25 17:58:30 +0100

    Fin TP4

--- SystemeFichiersVirtuel/MemoryManager.java
+++ SystemeFichiersVirtuel/MemoryManager.java
@@ -32,13 +32,15 @@ public class MemoryManager {
     private void initializeFilesystem() {
         writeSuperblock();
 
-        // TODO:
-        // Réserver les blocs système 0 à 128.
+		
+		for (int i = 0; i < 16; i++) {
+			memory[BITMAP_OFFSET + i] = (byte) 0xFF;
+		}	
+
+		memory[BITMAP_OFFSET + 16] = (byte) 0x01;
     }
 
     private void writeSuperblock() {
-        // TODO:
-        // Utiliser Utils pour écrire les métadonnées.
 
         Utils.writeString(
                 memory,
